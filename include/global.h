d044a044a368e83ea7aaa3bb1423fc70cc4937a7
miguellucietto7-alt <miguellucietto7@gmail.com>
2026-07-28 22:22:07 -0300

    organize everything, better documentation and comments, locate_project function, setup_project, implement setup, get and save project directory content and get_make_file_content()

--- include/global.h
+++ include/global.h
@@ -9,11 +9,18 @@
  * Mantém a inclusão das definições compartilhadas sem misturar
  * utilidades, constantes de comando e protótipos de funções.
  */
-#define INCLUDE \
+#define IncludeEveryThing \
 #include <stdio.h>\
 #include <stdlib.h>\
 #include <stdbool.h>\
 #include <string.h>\
+#include "global.h"\
+#include "utils.h"\
+#include "git.h"\
+#include "pdir.h"\
+#include "proj.h"\
+#include "cmd.h"\
+#include "commands.h"\
 
 
 #endif // _GLOBAL_H__
