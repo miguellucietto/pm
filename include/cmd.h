130a906e13130176b16acceffb6c46a3a061bf5c
miguel <miguellucietto7@gmail.com>
2026-08-04 11:40:45 -0300

    Restarting the project

--- include/cmd.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CMD_H_
-#define _CMD_H_
-
-#include "commands.h"
-
-void exec_cmd(CMD cmd);
-CMD get_cmd(int argc, char** argv);
-
-#endif // _CMD_H_
--- include/proj.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PROJ_H_
-#define _PROJ_H_
-
-void setup_project();
-void create_project(char* title);
-void locate_project(const char* name);
-
-#endif // _PROJ_H_
