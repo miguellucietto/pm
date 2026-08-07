bb396fcb6b8b3f8df344f63583958ab11c50efc2
miguel <miguellucietto7@gmail.com>
2026-08-07 10:27:14 -0300

    implemented -p flag function: accept the path of the project

--- src/add.c
+++ src/add.c
@@ -1,3 +1,5 @@
+#include <fenv.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +8,9 @@
 #include "shared.h"
 
 
+
+
+char *path;
 //\\// ----------------------------------------------------------------//\\//
 bool pmpath = false; // Accept the path of the pm project // -p
 bool verbose = false; // Print the process               // -v
@@ -21,30 +26,54 @@ int add_file(const char *name) {
   }
 
   FILE *f;
-  
+
+  // Verifying .pm
   if (!force) {
-    f = fopen(".pm", "r");
-    if (!f) {
-      printf("[INFO] Cannot add file outside of a pm project yet\n");
-      return EXIT_FAILURE;
+    if (pmpath) {
+      char opath[strlen(path) + 10];
+      
+      snprintf(opath, sizeof(opath), "%s/.pm", path);
+      f = fopen(opath, "r");
+      if (!f) {
+	printf("The path '%s' is not a pm project\n", path);
+	return EXIT_FAILURE;
+      }
+    } else {
+      f = fopen(".pm", "r");
+      if (!f) {
+	printf("The command has to be used in a pm project\n");
+	return EXIT_FAILURE;
+      }
     }
-    
-    
-    fclose(f);
+    fclose(f);    
   }
   
+
+    // Making paths      ↓
     if (verbose) puts("Making paths...");    
-    size_t s = strlen(name) + 100;
+    size_t s = strlen(name) + 1000;
     char fileh[s], filec[s];
-    snprintf(fileh, sizeof(fileh), "include/%s.h", name);
+
+    // Creating include path
+    if (pmpath) {
+      snprintf(fileh, sizeof(fileh), "%s/include/%s.h", path, name);
+    } else {
+      snprintf(fileh, sizeof(fileh), "include/%s.h", name);
+    }
     if (verbose) printf("Path %s made\n", fileh);
 
-    snprintf(filec, sizeof(filec), "src/%s.c", name);
+
+    // Creating source path
+    if (pmpath) {
+      snprintf(filec, sizeof(filec), "%s/src/%s.c", path, name);
+    } else {
+      snprintf(filec, sizeof(filec), "src/%s.c", name);
+    }
     if (verbose) printf("Path %s made\n", filec);
     
 
     if (verbose) puts("Creating files...");
-
+    // Creating source file
     if (!noc) {
       f = fopen(filec, "w");
       if (f) {
@@ -55,6 +84,7 @@ int add_file(const char *name) {
       }
     }
 
+    // Creating header file
     if (!noh) { 
       f = fopen(fileh, "w");
       if (f) {
@@ -70,10 +100,12 @@ int add_file(const char *name) {
 }
 
 
+// Verify flags and execute the function
 int pm_add(int argc, char **argv) {
   get_flags(argc, argv);
   if (has_flag('p')) {
     pmpath = true;
+    path = argv[argc - 1];
   }
   if (has_flag('v')) {
     verbose = true;
@@ -88,13 +120,17 @@ int pm_add(int argc, char **argv) {
     force = true;
   }
 
+  const char* flags = "phcvf";
+  warn_invalid_flags(strlen(flags), flags);
+  
   if (noh && noc) {
     if (verbose)
-      printf("\nBRUUUUUUUHHHHHH\n");
+      printf("-c and -h?\nBRUUUUUUUHHHHHH\n\n");
     return EXIT_SUCCESS;
   }
-  
+
   for (int i = 0; i < argc; i++) {
+    if (i == argc - 1) continue;
     if (!is_flag(argv[i]) && add_file(argv[i]) == EXIT_FAILURE) {
       return EXIT_FAILURE;
     }
